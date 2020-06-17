74e676e43d78460c3c11b91553a9181cbb84f2b3
cobbulso <jeongin1230@gmail.com>
2020-06-17 03:39:29 -0400

    Templates - Display the products in DB on the products page

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # /products -> index
 # Uniform Resource Locator (Address)
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+# Object, template, context: a dictionary that contains data to be pass to the template
 
 
 def new(request):
