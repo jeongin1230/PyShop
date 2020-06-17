9a0ae8e247e333d2fea0fb0ec24265f3b2b082a8
cobbulso <jeongin1230@gmail.com>
2020-06-17 00:36:18 -0400

    - Created an Offer model in models module (DB) - Created a superuser account - Log into admin page - Added a Products model - Added an orange to Products

--- products/models.py
+++ products/models.py
@@ -7,3 +7,8 @@ class Product(models.Model):
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083) # standard max_length for url
 
+
+class Offer(models.Model):
+    code = models.CharField(max_length=20)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
